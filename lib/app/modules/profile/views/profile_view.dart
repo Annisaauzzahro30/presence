df49361d09c746de30064ea9d022312691a7fbbb
Annisaauzzahro30 <ych4nisa444@gmail.com>
2024-06-26 15:10:23 +0700

    initial commit

--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -10,15 +11,79 @@ class ProfileView extends GetView<ProfileController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ProfileView'),
+        title: const Text('PROFILE'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'ProfileView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+      body: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+          stream: controller.streamUser(),
+          builder: (context, snap) {
+            if (snap.connectionState == ConnectionState.waiting) {
+              Center(child: CircularProgressIndicator());
+            }
+            if (snap.hasData) {
+              Map<String, dynamic> user = snap.data!.data()!;
+              return ListView(padding: EdgeInsets.all(20), children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ClipOval(
+                      child: Container(
+                        width: 100,
+                        height: 100,
+                        child: Image.network(
+                          "https://ui-avatars.com/api/?name=${user['name']}",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "${user['name'].toString().toUpperCase()}",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                Text(
+                  "${user['email']}",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ListTile(
+                  onTap: () {},
+                  leading: Icon(Icons.person),
+                  title: Text("Update Profile"),
+                ),
+                ListTile(
+                  onTap: () {},
+                  leading: Icon(Icons.vpn_key),
+                  title: Text("Update Password"),
+                ),
+                if (user["role"] == "admin")
+                  ListTile(
+                    onTap: () {},
+                    leading: Icon(Icons.person_add),
+                    title: Text("Add Pegawai"),
+                  ),
+                ListTile(
+                  onTap: () => controller.logout(),
+                  leading: Icon(Icons.logout),
+                  title: Text("Logout"),
+                ),
+              ]);
+            } else {
+              return Center(
+                child: Text("Tidak dapat memuat data user."),
+              );
+            }
+          }),
     );
   }
 }
