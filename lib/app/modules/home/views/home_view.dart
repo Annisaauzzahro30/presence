3dfd1a3802c7d51d95e5be6265eeb6fca29364e7
Annisaauzzahro30 <ych4nisa444@gmail.com>
2024-06-30 12:17:41 +0700

    initial commit

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,17 +1,21 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:presence/app/controllers/page_index_controller.dart';
 import 'package:presence/app/routes/app_pages.dart';
 import '../controllers/home_controller.dart';
 
+import '../controllers/page_index_controller.dart';
+
 class HomeView extends GetView<HomeController> {
-  const HomeView({super.key});
+  final pageC = Get.find<PageIndexController>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeView'),
+        title: const Text('HOME'),
         centerTitle: true,
         actions: [
           IconButton(
@@ -26,22 +30,15 @@ class HomeView extends GetView<HomeController> {
           style: TextStyle(fontSize: 20),
         ),
       ),
-      floatingActionButton: Obx(
-        () => FloatingActionButton(
-          onPressed: () async {
-            if (controller.isLoading.isFalse) {
-              controller.isLoading.value = true;
-              await FirebaseAuth.instance.signOut();
-              controller.isLoading.value = false;
-              Get.offAllNamed(Routes.LOGIN);
-            }
-          },
-          child: controller.isLoading.isFalse
-              ? const Icon(Icons.logout)
-              : const CircularProgressIndicator(
-                  color: Colors.white,
-                ),
-        ),
+      bottomNavigationBar: ConvexAppBar(
+        style: TabStyle.fixedCircle,
+        items: [
+          TabItem(icon: Icons.home, title: 'Home'),
+          TabItem(icon: Icons.fingerprint, title: 'Add'),
+          TabItem(icon: Icons.people, title: 'Profile'),
+        ],
+        initialActiveIndex: pageC.pageIndex.value,
+        onTap: (int i) => pageC.changePage(i),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:presence/app/controllers/page_index_controller.dart';
 import 'firebase_options.dart';
 import 'package:get/get.dart';
 
@@ -12,27 +12,27 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  final pageC = Get.put(PageIndexController(), permanent: true);
   runApp(
     StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return MaterialApp(
-              home: Scaffold(
-                body: Center(
-                  child: CircularProgressIndicator(),
-                ),
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
               ),
-            );
-          }
-          print(snapshot.data);
-          return DevicePreview(
-            builder: (contex) => GetMaterialApp(
-              title: "Application",
-              initialRoute: snapshot.data != null ? Routes.HOME : Routes.LOGIN,
-              getPages: AppPages.routes,
             ),
           );
-        }),
+        }
+        print(snapshot.data);
+        return GetMaterialApp(
+          title: "Application",
+          initialRoute: snapshot.data != null ? Routes.HOME : Routes.LOGIN,
+          getPages: AppPages.routes,
+        );
+      },
+    ),
   );
 }
